1aae3fea2ef2fd25a4a78418879553e319036feb
gokuer <johndoe@example.com>
2021-12-16 22:15:36 +0700

    Update code

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const path = require('path')
+
+app.use('/public', express.static(path.join(__dirname,'/public')))
+
+app.get('/', (req, res) => {
+    
+    
+    var file_locationnn = path.join(__dirname, '/public/home.html')
+    
+
+  res.sendFile(file_locationnn)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
